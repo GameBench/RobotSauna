bf71adff37a5e1caee2a1cca5de12fa6fbe55f18
Reuben Scratton <reuben.scratton@gmail.com>
2021-02-10 15:44:46 +0000

    Updates for 0.8

--- Assets/Script/GameBenchAPITest.cs
+++ Assets/Script/GameBenchAPITest.cs
@@ -10,6 +10,7 @@ public class GameBenchAPITest : MonoBehaviour
 
         Gamebench.SetTag("APITest", "true");
         Gamebench.SetTag("foo", "bar");
+        Gamebench.RemoveTag("foo");
 
         Invoke("StartSession", 5);
     }
@@ -17,27 +18,31 @@ public class GameBenchAPITest : MonoBehaviour
 
     void StartSession()
     {
+        Gamebench.Start("Script API Test");
 
-        Gamebench.Start();
-
-        // Enable power metrics which aren't normally collected that frequently
-        Gamebench.MetricEnable(MetricType.POW, 5);
-        Gamebench.MetricEnable(MetricType.BAT, 5);
+        // Enable some metrics which aren't normally collected that frequently
+        Gamebench.SetCaptureInterval(MetricType.POW, 5);
+        Gamebench.SetCaptureInterval(MetricType.BAT, 5);
+        Gamebench.SetCaptureInterval(MetricType.NET, 5);
 
         // Schedule some API calls and the end of the session
         Invoke("MarkLaunchComplete", 2);
         Invoke("MarkStart", 5);
         Invoke("MarkStop", 10);
-        Invoke("StopSession", 15);
+        Invoke("CaptureScreenshot", 20);
+        Invoke("StopSession", 30);
     }
     void MarkLaunchComplete() {
         Gamebench.MarkLaunchComplete();
     }
     void MarkStart() {
-        Gamebench.MarkStart("API Test Session");
+        Gamebench.MarkStart("API Test Marker 5s-10s");
     }
     void MarkStop() {
-        Gamebench.MarkStop("API Test Session");
+        Gamebench.MarkStop("API Test Marker 5s-10s");
+    }
+    void CaptureScreenshot() {
+        Gamebench.CaptureNow(MetricType.SS0);
     }
     void StopSession()
     {
